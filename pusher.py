33a7a63cbb23945919585b45ab2ccf0b670a045f
zeekay <zeekayy@gmail.com>
2011-11-04 14:23:33 +0000

    Trim trailing whitespace

--- pusher.py
+++ pusher.py
@@ -15,16 +15,16 @@ def hook(ui, repo, **kwargs):
 
 def push(path, repo):
     os.chdir(path)
-    logging.info(subprocess.check_output('hg up', shell=True))
+    logging.info(subprocess.check_output('hg up', shell=True).strip())
     try:
-        logging.info(subprocess.check_output('hg push -f ' + config['bitbucket']['repo-url'] + repo, shell=True))
+        logging.info(subprocess.check_output('hg push -f ' + config['bitbucket']['repo-url'] + repo, shell=True).strip())
         logging.info('push to bitbucket successful')
     except Exception as e:
         logging.error('push to bitbucket failed: %s' % str(e))
     if os.path.isdir(path + '/.git'):
         try:
             logging.info(subprocess.check_output('hg bookmark -f -r default master', shell=True))
-            logging.info(subprocess.check_output('hg push git+' + config['github']['repo-url'] + repo + '.git', shell=True))
+            logging.info(subprocess.check_output('hg push git+' + config['github']['repo-url'] + repo + '.git', shell=True).strip())
             logging.info('push to github successful')
         except Exception as e:
             logging.error('push to gitbub failed: %s' % str(e))
@@ -42,8 +42,7 @@ def server():
 
 if __name__ == '__main__':
     import logging, subprocess
-    logging.basicConfig(filename='pusher.log',level=logging.INFO)
-
+    logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument('--push')
     parser.add_argument('--server', action='store_true')
